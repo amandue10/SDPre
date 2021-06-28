7605010e2b24c49cde15a9bf63b9215c573bc227
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-06-28 16:08:09 -0600

    Sum numbers updated

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,9 +1,9 @@
 public class Objective7Lab4{
   public static void main(String[] args){
 
-    int num = 20, i = 1, sum = 0;
+    int count = 20, i = 1, sum = 0;
 
-    while(i <= num)
+    while(i <= count)
     {
       sum += i;
       i++;
