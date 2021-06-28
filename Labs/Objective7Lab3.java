cca050f94ccc07f1f6029238477eff4283cf8d78
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-06-28 15:02:04 -0600

    Identify odd and evens

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,20 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while(counter <= 20) {
+
+
+    if(counter % 2 == 0)
+    {
+      System.out.println(counter + " is Even");
+    }
+    else
+    {
+      System.out.println(counter + " is Odd" );
+    }
+
+    counter =  counter + 1;
+        }
+  }
+}
