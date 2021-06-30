c53d2c60b7b13530fe8cd27185f943fa7ea73946
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-06-29 21:38:22 -0600

    identify even and odds

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -2,14 +2,14 @@ public class Objective8Lab3 {
   public static void main(String[] args) {
     int counter = 1;
 
-    for(int i= 1;i<=20;i++){
+    for(counter= 1;counter<=20;counter++){
 
 
-      if(i % 2 == 0){
-        System.out.println(i + " is even ");
+      if(counter % 2 == 0){
+        System.out.println(counter + " is even ");
       }
       else {
-        System.out.println(i + " is odd");
+        System.out.println(counter + " is odd");
       }
     }
 
