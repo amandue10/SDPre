b551c7eb7c399ad115e8f924a9ae5ed6100db216
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-06-29 21:05:10 -0600

    sum numbers

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    int n = 20;
+    int sum = 0;
+    for(int i=1;i<=n;i++) {
+      sum = sum + i ;
+    }
+    System.out.println(sum);
+  }
+}
